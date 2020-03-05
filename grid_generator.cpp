dc56bb94eadacf2a51870ef6e18645c77b01fad5
David Xiao <davidx@Huogens-MacBook-Pro.local>
2020-03-05 10:04:02 -0500

    You can add traps

--- grid_generator.cpp
+++ grid_generator.cpp
@@ -6,8 +6,8 @@ using namespace std;
 //(warpTo 10@10) 2
 // (jump 10)3
 
-const int ROWS = 200;
-const int COLS = 200;
+const int ROWS = 10;
+const int COLS = 10;
 
 mt19937 rng;
 
@@ -33,6 +33,22 @@ void addLine(int r1, int c1, int r2, int c2, int val, int tempr, int tempc) {
     for (int i = min(r1, r2); i <= max(r1, r2); i++)
       grid[i][c1] = nVal;
   }
+}
+
+void addTrap(int r, int c, int midVal, int len, int wid) {
+  for (int i = r + 1; i < r + wid; i++) {
+    for (int j = c + 1; j < c + len; j++) {
+      grid[r][c] = to_string(midVal);
+    }
+  }
+  // vert
+  addLine(r, c, r+wid, c, 1, 1, 1);
+  addLine(r, c + len, r + wid, c + len, 1, 1, 1);
+  // hort
+  addLine(r, c, r, c + len, 1, 1, 1);
+  addLine(r + wid, c, r + wid, c + len, 1, 1, 1);
+
+
 }
 
 void printGrid() {
@@ -71,7 +87,19 @@ int main() {
       grid[r][c] = to_string(x);
     }
   }
-  addLine(0, 0, 0, 5, 1, 2, 2);
+  // add line from:
+  // row1, col1 to row2, col2 with a value, and other stuff.
+  // r1, c1, r2, c2, val, temp1, temp2
+  // temp1 will be used as value for jump squares
+  // temp1 and temp2 will be used as coordinates for warp squares
+  // 1 = death, 2 = warp, 3 = jump.
+  //addLine(0, 0, 0, 5, 1, 2, 2);
+
+  // add square trap:
+  // first 2 parameters are (r, c) for top left of square
+  // next val is the val you want on the inside
+  // next 2 vals are the length and width
+  //addTrap(0, 0, 3, 3, 5);
   printGrid();
 
   return 0;
